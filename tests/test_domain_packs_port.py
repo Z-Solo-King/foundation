9d3fb820d395c5cb967aa21eef72103c27de483b
Z-Solo-King <kingsoul0709@gmail.com>
2026-09-26 23:18:05 +0530

    test: cover 6369g non-Ipi-haze branch

--- tests/test_domain_packs_port.py
+++ tests/test_domain_packs_port.py
@@ -70,6 +70,7 @@ def test_unmatched_and_alternate_transform_paths():
 
 def test_special_weight_negative_branches_are_noops():
     assert apply_domain_rules("mouse", ["mouse"]) == {}
+    assert apply_domain_rules("6369g generic mouse", ["mouse"])["weight"] == "6369g"
     assert apply_domain_rules("ipi haze 6368g standard", ["mouse"])["weight"] == "6368g"
     assert apply_domain_rules("6369g standard mouse", ["mouse"])["weight"] == "6369g"
 
