fea62c04840ce841dfc5f8f83a214ee4d4a5dd37
Z-Solo-King <kingsoul0709@gmail.com>
2026-09-26 23:15:45 +0530

    test: cover special-rule nonmatch branch

--- tests/test_domain_packs_port.py
+++ tests/test_domain_packs_port.py
@@ -69,6 +69,7 @@ def test_unmatched_and_alternate_transform_paths():
 
 
 def test_special_weight_negative_branches_are_noops():
+    assert apply_domain_rules("mouse", ["mouse"]) == {}
     assert apply_domain_rules("ipi haze 6368g standard", ["mouse"])["weight"] == "6368g"
     assert apply_domain_rules("6369g standard mouse", ["mouse"])["weight"] == "6369g"
 
